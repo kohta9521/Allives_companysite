febb56e17835d89d13b29a24ffb6baa9b77c05e7
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-10-03 00:45:30 +0900

    14inchi move titlearea remake

--- last/script.js
+++ last/script.js
@@ -144,3 +144,19 @@ function drawSine(canvas, t, zoom, delay) {
 }
 
 init();
+
+
+//scroll_effect
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
--- sample/README.md
+++
@@ -1,1 +0,0 @@
-# Allives_companysite
